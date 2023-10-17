8eb659c7474c6e8ed0bc51bc5f65f4b5d947772c
Syeda Nabila Akter <68627327+Syeda-Nabila-Akter@users.noreply.github.com>
2023-10-17 18:19:56 +0600

    Create string-matching.py

---
+++ string-matching.py
@@ -0,0 +1,9 @@
+#Problem link: https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in range (len(stones)):
+            if stones[i] in jewels:
+                count += 1
+        return count
